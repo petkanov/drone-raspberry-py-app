1e4670c1f884fb8168c46b0dfa6dc5944ca66e79
Peter Petkanov <peter_black@angelic.com>
2021-03-02 13:05:18 +0200

    application configuration file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import logging, time, argparse
+import logging, time, argparse, configparser, sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--d', nargs=1, default=None)
 args = parser.parse_args()
 
 APP_DIR = args.d[0] if args.d != None else "./"
+CONFIGURATIONS = APP_DIR + 'configuration.ini'
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,5 +16,25 @@ logging.basicConfig(
     ]
 )
 
+config = configparser.ConfigParser()
+
+if len(config.read(CONFIGURATIONS)) == 0:
+    logging.error("Could Not Read Configurations File: " + CONFIGURATIONS)
+    sys.exit()  
+
+DRONE_ID = config['drone']['id']
+HOST_IP = config['cloud-app']['ip']
+VIDEO_PORT = int( config['cloud-app']['video-port'])
+
+GRAYSCALE = config['video']['grayscale'].lower() == 'true'
+FRAMES_PER_SECOND = int( config['video']['fps'])
+JPEG_QUALITY = int( config['video']['quality'])
+WIDTH = int( config['video']['width'])
+HEIGHT = int( config['video']['height'])
+
 if __name__ == '__main__':
     logging.debug('DroneApp has started! Directory %s', APP_DIR)
+
+    logging.info('FPS: %s  Quality: %s  Width %s Height %s  Grayscale: %s', 
+                 str(FRAMES_PER_SECOND), str(JPEG_QUALITY), str(WIDTH), str(HEIGHT), GRAYSCALE)
+    logging.info('Drone ID: %s  Video Recipient: %s:%s', str(DRONE_ID), str(HOST_IP), str(VIDEO_PORT))
